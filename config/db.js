a0f9aeaea4bc49af7d74dce6cb2bad83c14fda37
Tan06ishka <tanishkasalve06@gmail.com>
2026-06-16 14:48:14 +0530

    MongoDb Connection Process

--- config/db.js
+++ config/db.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        console.log("URI from db.js:", process.env.MONGO_URI);
+
+        await mongoose.connect(process.env.MONGO_URI);
+
         console.log("MongoDB Connected Successfully🔥");
-    }
-    catch (error) {
+    } catch (error) {
         console.error("MongoDB Connection Failed :", error.message);
         process.exit(1);
     }
